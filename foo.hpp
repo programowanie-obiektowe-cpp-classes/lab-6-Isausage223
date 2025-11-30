fbeb69550f436f49d12081beda2765b0a1998685
Isausage223 <kosowskipawel@yahoo.com>
2025-11-30 16:28:37 +0100

    1

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,11 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
-    // Twoja implementacja tutaj
+    std::transform(people.begin(), people.end(), retval.rbegin(),
+                   [](Human& h) {
+                       h.birthday();                      // zwiększamy wiek
+                       return h.isMonster() ? 'n' : 'y';   // 'n' jeśli potwór, 'y' w przeciwnym razie
+                   });
 
     return retval;
 }
